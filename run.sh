69c2fb235c5a2153b7e17354f2f4fca8a6a3a458
root <root@pclpsserrew1cit.services.brown.edu>
2020-08-19 11:26:54 -0400

    add email to sync container

--- run.sh
+++ run.sh
@@ -1,6 +1,34 @@
 #/bin/bash
+
+apk add ssmtp
+
+cat > /etc/ssmtp/ssmtp.conf << EOF
+root=postmaster
+mailhub=mail-relay.brown.edu:25
+hostname=`hostname`
+FromLineOverride=YES
+EOF
+
+cat > /drew.txt << EOF
+To: drew_linsley@brown.edu
+From: connectomics@brown.edu
+Subject: Connectomics Sync Complete
+
+Connectomics Sync Complete
+EOF
+
+cat > /pb.txt << EOF
+To: pb@brown.edu
+From: connectomics@brown.edu
+Subject: Connectomics Sync Complete
+
+Connectomics Sync Complete
+EOF
+
 while true
 do
 [ -f /src/sync_me ] && ( rsync -avh --delete-before /src/ /dest && rm -f /src/sync_me )
+ssmtp drew_linsley@brown.edu < /drew.txt
+ssmtp pb@brown.edu < /pb.txt
 sleep 60
 done
